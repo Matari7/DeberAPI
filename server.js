14f77a8a619c308acfb68ae86d6a62ae1c282d19
Matari7 <arimcf10@hotmail.es>
2024-05-26 15:00:19 -0500

    rest y graphql

--- server.js
+++ server.js
@@ -1,38 +1,49 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const { typeDefs, resolvers } = require('./graphqlController');
 const restController = require('./restController');
-const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-// Configurar Apollo Server
-const server = new ApolloServer({ typeDefs, resolvers });
-await server.start();
-server.applyMiddleware({ app });
+async function startServer() {
+    // Configurar Apollo Server
+    const server = new ApolloServer({ typeDefs, resolvers });
+    await server.start();
+    server.applyMiddleware({ app });
 
-// Usar el enrutador de restController
-app.use('/api', restController);
+    // Ruta para servir el archivo index.html
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    });
 
+    // Ruta para servir el archivo rest.html
+    app.get('/restTest', (req, res) => {
+        res.sendFile(path.join(__dirname, 'restTest.html'));
+    });
 
-// Ruta para servir el archivo index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+    // Ruta para servir el archivo graphql.html
+    app.get('/graphqlTest', (req, res) => {
+        res.sendFile(path.join(__dirname, 'graphqlTest.html'));
+    });
 
-// Manejar rutas no encontradas
-app.use((req, res, next) => {
-    res.status(404).send('La ruta no fue encontrada');
-});
+    // Usar el enrutador de restController
+    app.use('/api', restController);
 
-app.get('/index', (req, res)=>{
-    res.render('index');
-})
+    // Manejar rutas no encontradas
+    app.use((req, res, next) => {
+        res.status(404).send('La ruta no fue encontrada');
+    });
 
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+    app.listen(port, () => {
+        console.log(`Servidor escuchando en http://localhost:${port}`);
+    });
+}
+
+startServer().catch((error) => {
+    console.error('Error starting the server:', error);
 });
